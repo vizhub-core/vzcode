35264dcc9aa4e01584c1b9162c51b178ac4183c3
Curran Kelleher <curran.kelleher@gmail.com>
2023-02-02 12:39:27 -0500

    Adopt React patterns, part 1.

--- src/App.jsx
+++ src/App.jsx
@@ -1,110 +1,115 @@
-import { useState, useEffect } from 'react';
-import './App.css';
-import './style.css';
-import ShareDBClient from 'sharedb-client-browser/sharedb-client-json1-browser.js';
+import { useState, useEffect } from "react";
+import "./App.css";
+import "./style.css";
+import ShareDBClient from "sharedb-client-browser/sharedb-client-json1-browser.js";
 
 const { Connection } = ShareDBClient;
-const socket = new WebSocket('ws://' + window.location.host);
+const socket = new WebSocket("ws://" + window.location.host);
 const connection = new Connection(socket);
 
 function App() {
   const [data, setData] = useState(null);
 
+  const [isFileMenuOpen, setIsFileMenuOpen] = useState(false);
+
   useEffect(() => {
-    const doc = connection.get('documents', '1');
+    const doc = connection.get("documents", "1");
 
     doc.subscribe(() => {
       setData(doc.data);
     });
   }, []);
 
+  // let parsedData = {};
+  // // // Gets the files from the ShareDB structure and fills them into the sidebar
+  // function fillData() {
+  //   let fileList = document.querySelector(".sub-menu");
+  //   let i = 0;
+  //   for (var key in data) {
+  //     let file = JSON.stringify(data[key].name);
+  //     let content = JSON.stringify(data[key].text);
+  //     file = file.replace(/['"]+/g, "");
+  //     fileList.innerHTML += "<li><a>" + file + "</a></li>";
+  //     parsedData[i] = {
+  //       name: file,
+  //       content: content,
+  //     };
+  //     i++;
+  //   }
+  // }
 
-  function showFiles() {
-    const arrow = document.querySelectorAll(".arrow");
-    for (var i = 0; i < arrow.length; i++) {
-      arrow[i].addEventListener("click", (e) => {
-        let arrowParent = e.target.parentElement.parentElement;//selecting main parent of arrow
-        arrowParent.classList.toggle("showMenu");
-        let folder = document.getElementById("folderIcon");
-        if (folder.className === "bx bx-folder-open") {
-          folder.className = "bx bx-folder";
-        } else {
-          folder.className = "bx bx-folder-open";
-        }
-      });
-    }
-  }
+  // // Lets you click on a file.
+  // function showContent() {
+  //   let file = document.querySelectorAll(".sub-menu li");
+  //   for (var i = 0; i < file.length; i++) {
+  //     file[i].addEventListener("click", (e) => {
+  //       let fileName = e.target.innerHTML;
+  //       let text = document.getElementById("edit");
+  //       for (var key in parsedData) {
+  //         if (parsedData[key].name === fileName) {
+  //           var content = parsedData[key].content.replace('\n', '<br />');
+  //           text.value = content;
+  //         }
+  //       }
+  //     });
+  //   }
+  // }
 
-  let parsedData = {};
-
-  function fillData() {
-    let fileList = document.querySelector(".sub-menu");
-    let i = 0;
-    for (var key in data) {
-      let file = JSON.stringify(data[key].name);
-      let content = JSON.stringify(data[key].text);
-      file = file.replace(/['"]+/g, '');
-      fileList.innerHTML += '<li><a>' + file + '</a></li>'
-      parsedData[i] = {
-        name: file,
-        content: content,
-      }
-      i++;
-    }
-  }
-
-  function showContent() {
-    let file = document.querySelectorAll(".sub-menu li");
-    for (var i = 0; i < file.length; i++) {
-      file[i].addEventListener("click", (e) => {
-        let fileName = e.target.innerHTML;
-        let text = document.getElementById("edit");
-        for (var key in parsedData) {
-          if (parsedData[key].name === fileName) {
-            var content = parsedData[key].content.replace('\n', '<br />');
-            text.value = content;
-          }
-        }
-      });
-    }
-  }
-
-
-  fillData();
-  showContent();
-
+  //fillData();
+  // showContent();
 
   return (
     <>
-      <div className="tabList"></div><div className="bottomBar"></div><div className="sidebar show">
-
+      <div className="tabList"></div>
+      <div className="bottomBar"></div>
+      <div className="sidebar show">
         <ul className="nav-links">
-          <li>
-            <div className="iocn-link">
+          <li className={isFileMenuOpen ? "showMenu" : ""}>
+            <div className="icon-link">
               <a href="#">
-                <i id="folderIcon" className='bx bx-folder'></i>
+                <i
+                  id="folderIcon"
+                  className={
+                    isFileMenuOpen ? "bx bx-folder-open" : "bx bx-folder"
+                  }
+                ></i>
                 <span className="link_name">Files</span>
               </a>
-              <i className='bx bxs-chevron-down arrow' onClick={showFiles()}></i>
+              <i
+                className="bx bxs-chevron-down arrow"
+                onClick={(e) => {
+                  setIsFileMenuOpen(!isFileMenuOpen);
+                }}
+              ></i>
             </div>
             <ul className="sub-menu">
-              <li><a className="link_name" href="#">Files</a></li>
+              <li>
+                <a className="link_name" href="#">
+                  Files
+                </a>
+              </li>
             </ul>
           </li>
           <li>
             <div className="profile-details">
               <a href="#">
-                <i className='bx bx-cog'></i>
+                <i className="bx bx-cog"></i>
                 <span className="link_name">Setting</span>
               </a>
               <ul className="sub-menu blank">
-                <li><a className="link_name" href="#">Setting</a></li>
+                <li>
+                  <a className="link_name" href="#">
+                    Setting
+                  </a>
+                </li>
               </ul>
             </div>
           </li>
         </ul>
       </div>
-      <textarea className="TextEdit" name="editor" id="edit">Hello</textarea>
+      <textarea className="TextEdit" name="editor" id="edit">
+        Hello
+      </textarea>
     </>
   );
 }
