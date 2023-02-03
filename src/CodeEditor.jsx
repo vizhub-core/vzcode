855f3b7c617e6c5f20c30ece4ebe20ad917ad510
Anooj Pai <anooj8000@gmail.com>
2023-02-03 16:57:55 -0500

    File extension syntax highlight and misc styling

--- src/CodeEditor.jsx
+++ src/CodeEditor.jsx
@@ -1,40 +1,50 @@
 import { useRef, useLayoutEffect } from 'react';
 import { EditorView, basicSetup } from "codemirror"
 import { EditorState } from "@codemirror/state"
-import { javascript } from "@codemirror/lang-javascript"
+import { javascript } from "@codemirror/lang-javascript";
+import { html } from "@codemirror/lang-html";
 import { oneDark } from '@codemirror/theme-one-dark';
 
 // Keys are file ids
 // Values are CodeMirror instances
 const editorCache = new Map();
 
+
 // Gets or creates a CodeMirror editor for the given file id.
 const getOrCreateEditor = (fileId, data) => {
+
+    const fileExtension = data[fileId].name.split('.').pop();
+
+    const extensions = [
+        // json1Sync({ shareDBDoc, path, debug }),
+        // vizhubHighlightStyle,
+        ///...(fileExtension && fileExtension in langByFileExtension
+        ///  ? [langByFileExtension[fileExtension]()]
+        ///  : []),
+        basicSetup,
+        oneDark
+    ];
+
+    if (fileExtension === 'js') {
+        extensions.push(javascript())
+    } else if (fileExtension === 'html') {
+        extensions.push(html())
+    }
+
+
     let editor = editorCache.get(fileId);
     if (!editor) {
         // Cache miss --> mint a new editor.
         editor = new EditorView({
             state: EditorState.create({
                 doc: data[fileId].text,
-                extensions: [
-                    // json1Sync({ shareDBDoc, path, debug }),
-                    // vizhubHighlightStyle,
-                    ///...(fileExtension && fileExtension in langByFileExtension
-                    ///  ? [langByFileExtension[fileExtension]()]
-                    ///  : []),
-                    basicSetup,
-                    oneDark,
-                    javascript()
-                ],
+                extensions
             }),
         });
 
         // Populate the cache.
         editorCache.set(fileId, editor);
 
-        // if (debug) {
-        console.log('Cache miss, minted new editor for fileId ' + fileId + '.');
-        // }
     }
     return editor;
 };
@@ -48,8 +58,6 @@ export const CodeEditor = ({
     // useEffect was buggy in that sometimes ref.current was undefined.
     // useLayoutEffect seems to solve that issue.
     useLayoutEffect(() => {
-        console.log('activeFileId = ' + activeFileId);
-
         const editor = getOrCreateEditor(activeFileId, data);
         ref.current.appendChild(editor.dom);
 
