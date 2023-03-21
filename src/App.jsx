781faaca7c9f52cb37ebd8ce7146e6f61736620b
Curran <curran.kelleher@gmail.com>
2023-03-21 02:01:41 -0400

    Add vite link for docs

--- src/App.jsx
+++ src/App.jsx
@@ -18,6 +18,7 @@ const socket = new WebSocket('ws://' + window.location.host + '/ws');
 const connection = new Connection(socket);
 
 // A feature flag to disable settings for release, until it's completed.
+// See https://vitejs.dev/guide/env-and-mode.html
 const disableSettings = import.meta.env.VITE_DISABLE_SETTINGS === 'true';
 
 function App() {
