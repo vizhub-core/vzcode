ee4ab187c8f6c7afcf572e1b37cc0ac257b9e7a9
Curran <curran.kelleher@gmail.com>
2023-04-25 03:00:07 -0400

    Add failing test cases for directories

--- test/listFiles.test.js
+++ test/listFiles.test.js
@@ -4,22 +4,73 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 import { computeInitialDocument } from '../src/server/computeInitialDocument';
 
+const getSampleFiles = (sampleDirectory) => {
+  const filename = fileURLToPath(import.meta.url);
+  const dirname = path.dirname(filename);
+  const fullPath = path.join(dirname, 'sampleDirectories', sampleDirectory);
+  const initialDocument = computeInitialDocument({ fullPath });
+
+  // Sort files for stability of tests, as ordering of fs listing
+  // is not guaranteed.
+  const files = Object.values(initialDocument).sort((a, b) =>
+    ascending(a.name, b.name)
+  );
+
+  return files;
+};
+
 describe('Listing files', () => {
-  it('should list files', () => {
-    const filename = fileURLToPath(import.meta.url);
-    const dirname = path.dirname(filename);
-    const fullPath = path.join(dirname, 'sampleDirectories', 'listFiles');
-    const initialDocument = computeInitialDocument({ fullPath });
+  it('should list files, no directories', () => {
+    expect(getSampleFiles('listFiles')).toEqual([
+      {
+        text: '<!DOCTYPE html>\n<html>\n  <head>\n    <meta charset="utf-8" />\n    <meta namde="viewport" content="width=device-width" />\n    <title>Test Page</title>\n  </head>\n  <body></body>\n</html>\n',
+        name: 'index.html',
+      },
+      {
+        text: "console.log('This is a test file to edit');\n",
+        name: 'index.js',
+      },
+      {
+        text: 'body {\n    background-color: red;\n}',
+        name: 'styles.css',
+      },
+    ]);
+  });
 
-    // Sort files for stability of tests, as ordering of fs listing
-    // is not guaranteed.
-    const files = Object.values(initialDocument).sort((a, b) =>
-      ascending(a.name, b.name)
-    );
+  it('should list files, empty directory', () => {
+    expect(getSampleFiles('listFilesEmptyDirectory')).toEqual([
+      {
+        name: 'emptyDirectory/',
+      },
+      {
+        text: '<!DOCTYPE html>\n<html>\n  <head>\n    <meta charset="utf-8" />\n    <meta namde="viewport" content="width=device-width" />\n    <title>Test Page</title>\n  </head>\n  <body></body>\n</html>\n',
+        name: 'index.html',
+      },
+      {
+        text: "console.log('This is a test file to edit');\n",
+        name: 'index.js',
+      },
+      {
+        text: 'body {\n    background-color: red;\n}',
+        name: 'styles.css',
+      },
+    ]);
+  });
 
-    // console.log(JSON.stringify(files, null, 2));
-
-    expect(files).toEqual([
+  it('should list files, full directory', () => {
+    expect(getSampleFiles('listFilesEmptyDirectory')).toEqual([
+      {
+        text: '<!DOCTYPE html>\n<html>\n  <head>\n    <meta charset="utf-8" />\n    <meta namde="viewport" content="width=device-width" />\n    <title>Test Page</title>\n  </head>\n  <body></body>\n</html>\n',
+        name: 'emptyDirectory/index.html',
+      },
+      {
+        text: "console.log('This is a test file to edit');\n",
+        name: 'emptyDirectory/index.js',
+      },
+      {
+        text: 'body {\n    background-color: red;\n}',
+        name: 'emptyDirectory/styles.css',
+      },
       {
         text: '<!DOCTYPE html>\n<html>\n  <head>\n    <meta charset="utf-8" />\n    <meta namde="viewport" content="width=device-width" />\n    <title>Test Page</title>\n  </head>\n  <body></body>\n</html>\n',
         name: 'index.html',
