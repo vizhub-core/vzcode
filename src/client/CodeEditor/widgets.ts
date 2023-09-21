0003b1fdbea0db935c3f311812846558a140d222
Darmin-Geek <34427428+Darmin-Geek@users.noreply.github.com>
2023-09-21 10:37:09 -0400

    Added rgb color picker and vec2 drag widget

--- src/client/CodeEditor/widgets.ts
+++ src/client/CodeEditor/widgets.ts
@@ -43,6 +43,59 @@ export const widgets = ({
           }
         },
       },
+      //vec2 slider
+      {
+        regexp:
+          /vec2\(-?\b\d+\.?\d*\b\s*(,\s*-?\b\d+\.?\d*\b)?\)/g,
+        cursor: 'move',
+
+        onDrag: (text, setText, e) => {
+          const res =
+            /vec2\((?<x>-?\b\d+\.?\d*\b)\s*(,\s*(?<y>-?\b\d+\.?\d*\b))?\)/.exec(
+              text,
+            );
+          let x = Number(res?.groups?.x);
+          let y = Number(res?.groups?.y);
+          if (isNaN(x)) return;
+          if (isNaN(y)) y = x;
+          setText(
+            `vec2(${x + e.movementX}, ${y - e.movementY})`,
+          );
+        },
+      },
+      //color picker
+      //TODO: create color picker for hsl colors
+      {
+        regexp: /rgb\(.*\)/g,
+        cursor: 'pointer',
+        onClick: (text, setText, e) => {
+          const res =
+            /rgb\((?<r>\d+)\s*,\s*(?<g>\d+)\s*,\s*(?<b>\d+)\)/.exec(
+              text,
+            );
+          const r = Number(res?.groups?.r);
+          const g = Number(res?.groups?.g);
+          const b = Number(res?.groups?.b);
+
+          //sel will open the color picker when sel.click is called.
+          const sel = document.createElement('input');
+          sel.type = 'color';
+          if (!isNaN(r + g + b))
+            sel.value = rgb2Hex(r, g, b);
+
+          const updateRGB = (e: Event) => {
+            const el = e.target as HTMLInputElement;
+            if (el.value) {
+              const [r, g, b] = hex2RGB(el.value);
+              setText(`rgb(${r}, ${g}, ${b})`);
+            }
+            sel.removeEventListener('change', updateRGB);
+          };
+
+          sel.addEventListener('change', updateRGB);
+          sel.click();
+        },
+      },
       // url clicker
       {
         regexp: /https?:\/\/[^ "]+/g,
@@ -53,3 +106,11 @@ export const widgets = ({
       },
     ],
   });
+
+const hex2RGB = (hex: string): [number, number, number] => {
+  const v = parseInt(hex.substring(1), 16);
+  return [(v >> 16) & 255, (v >> 8) & 255, v & 255];
+};
+
+const rgb2Hex = (r: number, g: number, b: number): string =>
+  '#' + r.toString(16) + g.toString(16) + b.toString(16);
---
+++ test/sampleDirectories/kitchenSink/widgetTest1.js
@@ -0,0 +1,12 @@
+//Assume relevant modules (potentially from D3) are imported
+
+let aColor = rgb(81, 89, 194);
+
+let b = vec2(181, 110);
+/*
+
+
+*/
+let a = rgb(236, 24, 24);
+
+let c = false;
