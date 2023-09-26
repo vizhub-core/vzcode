2775b0f6a0313d9c25b5200c4b7a09e701ee735c
Curran <curran.kelleher@gmail.com>
2023-09-26 19:38:16 -0400

    Misc cleanup

--- src/client/Sidebar/Item.tsx
+++ src/client/Sidebar/Item.tsx
@@ -9,46 +9,34 @@ export const Item = ({
   handleDeleteClick,
   isDir,
 }) => {
+  // Tracks whether the mouse is hovering over the file or directory
   const [isHovered, setIsHovered] = useState(false);
 
+  // Tracks whether the file is being renamed (inline text input)
   const [isRenaming, setIsRenaming] = useState(false);
 
+  // Tracks the value of the input field when renaming
   const [renameValue, setRenameValue] = useState('');
 
   const onKeyDown = useCallback((event) => {
+    // TODO make 'Escape' cancel the changes - do not commit the rename
+    // https://github.com/vizhub-core/vzcode/issues/207
     if (event.key === 'Enter' || event.key === 'Escape') {
       event.target.blur();
     }
   }, []);
 
-  const onBlur = useCallback((event) => {
-    if (event.target.value.trim() === '') {
-      // setEditingValue(value);
-      console.log('TODO: handle empty rename');
-    } else {
-      setIsRenaming(false);
-      handleRenameClick(event.target.value);
-    }
-  }, []);
-  const getDisplayMode = (renaming) =>
-    isRenaming ? (
-      <input
-        type="text"
-        aria-label="Field name"
-        value={renameValue}
-        onKeyDown={onKeyDown}
-        onBlur={onBlur}
-        onChange={(event) =>
-          setRenameValue(event.target.value)
-        }
-      />
-    ) : (
-      children
-    );
-
-  const displayMode = useMemo(
-    (isRenaming) => getDisplayMode(isRenaming),
-    [isRenaming, renameValue],
+  const onBlur = useCallback(
+    (event: React.FocusEvent<HTMLInputElement>) => {
+      if (event.target.value.trim() === '') {
+        // setEditingValue(value);
+        console.log('TODO: handle empty rename');
+      } else {
+        setIsRenaming(false);
+        handleRenameClick(event.target.value);
+      }
+    },
+    [handleRenameClick],
   );
 
   const [showModal, setShowModal] = useState(false);
@@ -75,8 +63,14 @@ export const Item = ({
 
   const handleMouseLeave = useCallback(() => {
     setIsHovered(false);
-    setIsRenaming(false);
   }, []);
+
+  const onChange = useCallback(
+    (event: React.ChangeEvent<HTMLInputElement>) => {
+      setRenameValue(event.target.value);
+    },
+    [],
+  );
 
   return (
     <div
@@ -85,7 +79,20 @@ export const Item = ({
       onMouseEnter={handleMouseEnter}
       onMouseLeave={handleMouseLeave}
     >
-      <div className="name">{displayMode}</div>
+      <div className="name">
+        {isRenaming ? (
+          <input
+            type="text"
+            aria-label="Field name"
+            value={renameValue}
+            onKeyDown={onKeyDown}
+            onBlur={onBlur}
+            onChange={onChange}
+          />
+        ) : (
+          children
+        )}
+      </div>
       {isHovered ? (
         <div className="utils">
           <i
