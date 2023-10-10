bc0598a93a4a1b4d5672f362e8e0f6d8328a9aba
Curran <curran.kelleher@gmail.com>
2023-10-10 17:29:19 -0400

    Progress

--- src/client/PresenceNotifications/index.tsx
+++ src/client/PresenceNotifications/index.tsx
@@ -1,52 +1,128 @@
 import { useState, useEffect } from 'react';
-import { Modal} from '../bootstrap';
 import './style.scss';
 
+// A presence notification is a message that is displayed when a user joins or leaves a session.
+// It has the following properties:
+// - `user` is the username of the user who joined or left the session.
+// - `join` is a boolean value that indicates whether the user joined or left the session.
+//   - `true` means user has joined the session.
+//   - `false` means user has left the session.
+type PresenceNotification = {
+  user: string;
+  join: boolean;
+  presenceId: string;
+};
+
 export const PresenceNotifications = ({
-  docPresence
+  docPresence,
 }: {
   docPresence?: any;
 }) => {
-    // console.log('docPresence:', docPresence);
-    const [visible, setVisible] = useState(true);
-    const [user, setUser] = useState('');
-    const [join, setJoin] = useState(true);
+  const [presenceNotifications, setPresenceNotifications] =
+    useState<Array<PresenceNotification>>([]);
 
-    useEffect(() => {
-      // console.log('useEffect triggered.');
-      if (docPresence) {
-        // console.log('inside if of useEffect.');
-        docPresence.on('receive', (presence) => {
-          if (presence === null){
-            setJoin(false);
-          }
-          const {username} = presence;
-          setUser(username);
-        });
+  // // console.log('docPresence:', docPresence);
+  // const [visible, setVisible] = useState(true);
+  // const [user, setUser] = useState('');
 
-        const timer = setTimeout(() => {
-          setVisible(false);
-        }, 10000);
-        return () => {
-          clearTimeout(timer);
-        };
-      } else {
-        setVisible(false);
-      }
-    }, [docPresence]);
-    
-    
+  // // `true` means user has joined the session.
+  // // `false` means user has left the session.
+  // const [join, setJoin] = useState<boolean>(true);
 
-    return (
-        <Modal
-          className="vz-notification"
-          show={visible}
-          onHide={() => setVisible(false)}
-          animation={false}
-        >
-          <Modal.Body>
-            {join ? `${user} has joined the session.` : `${user} has left the session.`}
-          </Modal.Body>
-        </Modal>
-    );
+  useEffect(() => {
+    // console.log('useEffect triggered.');
+    if (docPresence) {
+      // See https://share.github.io/sharedb/presence
+      docPresence.on('receive', (presenceId, update) => {
+        // TODO figure out how to get username from `presenceId`.
+        const user = 'someone';
+
+        // `true` means user has joined the session.
+        // `false` means user has left the session.
+        const join = update === null;
+
+        // Add an entry to the presence notifications array.
+        setPresenceNotifications((prev) => [
+          ...prev,
+          { user, join, presenceId },
+        ]);
+
+        // Remove it after 5 seconds.
+        setTimeout(() => {
+          setPresenceNotifications((prev) =>
+            prev.filter(
+              (notification) =>
+                notification.presenceId !== presenceId,
+            ),
+          );
+        }, 5000);
+      });
+
+      // console.log('inside if of useEffect.');
+      // docPresence.on('receive', (presence) => {
+      //   console.log('presence', presence);
+
+      // // Add an entry to the presence notifications array.
+      // setPresenceNotifications((prev) => [
+      //   ...prev,
+      //   { user: presence.username, join: presence === null },
+      // ]);
+
+      // console.log('receive triggered.');
+      // if (presence === null) {
+      //   setJoin(false);
+      // }
+      // const { username } = presence;
+      // setUser(username);
+      // });
+
+      // const timer = setTimeout(() => {
+      //   setVisible(false);
+      // }, 10000);
+      return () => {
+        // clearTimeout(timer);
+      };
+    } else {
+      // setVisible(false);
+    }
+  }, [docPresence]);
+
+  console.log(
+    'presenceNotifications',
+    presenceNotifications,
+  );
+
+  return presenceNotifications.length > 0 ? (
+    <div className="vz-notification">
+      {presenceNotifications.map((notification) => {
+        const { user, join } = notification;
+        return (
+          <div
+            key={notification.presenceId}
+            className="vz-notification-item"
+          >
+            {join
+              ? `${user} has joined the session.`
+              : `${user} has left the session.`}
+          </div>
+        );
+      })}
+    </div>
+  ) : null;
+  // return null;
+
+  // return (
+  //   <Modal
+  //     className="vz-notification"
+  //     show={visible}
+  //     onHide={() => setVisible(false)}
+  //     animation={false}
+  //   >
+  //     <Modal.Body>
+  //       {join
+  //         ? `${user} has joined the session.`
+  //         : `${user} has left the session.`}
+  //     </Modal.Body>
+  //   </Modal>
+  // );
 };
