ba48d96f2c82bad012f6d1ac5e4c98cebba351e6
Curran <curran.kelleher@gmail.com>
2023-10-26 17:52:30 -0400

    Overhaul PresenceNotifications

--- src/client/PresenceNotifications/index.tsx
+++ src/client/PresenceNotifications/index.tsx
@@ -2,16 +2,69 @@ import { useState, useEffect, useRef } from 'react';
 import { PresenceId, Username } from '../../types';
 import './style.scss';
 
-// A presence notification is a message that is displayed when a user joins or leaves a session.
-// It has the following properties:
-// - `user` is the username of the user who joined or left the session.
-// - `join` is a boolean value that indicates whether the user joined or left the session.
-//   - `true` means user has joined the session.
-//   - `false` means user has left the session.
 type PresenceNotification = {
   user: Username;
   join: boolean;
   presenceId: PresenceId;
+};
+
+export const extractTimestampFromId = (
+  id: string,
+): number => {
+  const [timestampPart] = id.split('-');
+  return parseInt(timestampPart, 36);
+};
+
+export const shouldShowNotification = (
+  remotePresenceId: PresenceId,
+  localPresenceId: PresenceId,
+): boolean => {
+  const remotePresenceTimestamp = extractTimestampFromId(
+    remotePresenceId,
+  );
+  const localPresenceTimestamp =
+    extractTimestampFromId(localPresenceId);
+  return remotePresenceTimestamp > localPresenceTimestamp;
+};
+
+export const handleJoin = (
+  presenceId: PresenceId,
+  username: Username,
+  alreadyJoined: React.MutableRefObject<
+    Map<PresenceId, Username>
+  >,
+  setPresenceNotifications: any,
+) => {
+  if (!alreadyJoined.current.has(presenceId)) {
+    alreadyJoined.current.set(presenceId, username);
+    setPresenceNotifications(
+      (prev: PresenceNotification[]) => [
+        ...prev,
+        { user: username, join: true, presenceId },
+      ],
+    );
+  } else {
+    alreadyJoined.current.set(presenceId, username); // Update in case username changed
+  }
+};
+
+export const handleLeave = (
+  presenceId: PresenceId,
+  alreadyJoined: React.MutableRefObject<
+    Map<PresenceId, Username>
+  >,
+  setPresenceNotifications: any,
+) => {
+  const user = alreadyJoined.current.get(presenceId);
+  if (user) {
+    alreadyJoined.current.delete(presenceId);
+    setPresenceNotifications(
+      (prev: PresenceNotification[]) => [
+        ...prev,
+        { user, join: false, presenceId },
+      ],
+    );
+  }
 };
 
 export const PresenceNotifications = ({
@@ -21,101 +74,85 @@ export const PresenceNotifications = ({
   docPresence?: any;
   localPresence?: any;
 }) => {
-  // Keep track of the presence notifications that are displayed
+  // `presenceNotifications`:
+  // - State variable containing a list of user presence notifications.
+  // - Each notification indicates if a user has joined or left a session.
   const [presenceNotifications, setPresenceNotifications] =
-    useState<Array<PresenceNotification>>([]);
+    useState<PresenceNotification[]>([]);
 
-  // Keep track of the presence IDs and its respective username that have already joined the session
-  // - Switched from Set to Map to keep track of usernames
-  //   - PresenceId is the key
-  //   - Username is the value
-  //   - Username is stored in each remote presence, so we can't use docPresence.remotePresences[presenceId].username
-  //     to get the username for when the user leaves the session because the remote presence would have already been removed
+  // `alreadyJoined`:
+  // - Ref (persistent across re-renders) containing a Map.
+  // - Map's key: `PresenceId` (unique identifier for user's presence).
+  // - Map's value: `Username` of the associated user.
+  // - Tracks users who have joined the session and their possible username changes.
   const alreadyJoined = useRef<Map<PresenceId, Username>>(
     new Map(),
   );
 
-  const extractTimestampFromId = (id) => {
-    const [timestampPart] = id.split('-');
-    return parseInt(timestampPart, 36);
-  };
-
   useEffect(() => {
     if (docPresence) {
-      // See https://share.github.io/sharedb/presence
-      docPresence.on(
-        'receive',
-        (presenceId: PresenceId, update) => {
-          // `true` means user has joined the session.
-          // `false` means user has left the session.
-          const join = update !== null;
-
-          // Get the timestamps from the presence IDs to see whether the remote presence
-          // joined before or after the local presence.
-          const remotePresenceTimestamp =
-            extractTimestampFromId(presenceId);
-          const localPresenceTimestamp =
-            extractTimestampFromId(
-              localPresence.presenceId,
-            );
-
-          // If the remote presence joined before the local presence, then we don't need to show a notification.
-          if (
-            remotePresenceTimestamp <=
-            localPresenceTimestamp
+      const handleReceive = (
+        presenceId: PresenceId,
+        update: any,
+      ) => {
+        const join = update !== null;
+        if (
+          shouldShowNotification(
+            presenceId,
+            localPresence.presenceId,
           )
-            return;
-
+        ) {
           if (join) {
-            // Figure out if we have ever seen this user before.
-            const user =
+            const username =
               docPresence.remotePresences[presenceId]
-                .username;
-            if (!alreadyJoined.current.has(presenceId)) {
-              alreadyJoined.current.set(presenceId, user);
-              setPresenceNotifications((prev) => [
-                ...prev,
-                { user, join, presenceId },
-              ]);
-            } else {
-              // Update the username in case it changed
-              alreadyJoined.current.set(presenceId, user);
-            }
-          } else if (!join) {
-            const user =
-              alreadyJoined.current.get(presenceId);
-            alreadyJoined.current.delete(presenceId);
-            setPresenceNotifications((prev) => [
-              ...prev,
-              { user, join, presenceId },
-            ]);
+                ?.username;
+            handleJoin(
+              presenceId,
+              username,
+              alreadyJoined,
+              setPresenceNotifications,
+            );
+          } else {
+            handleLeave(
+              presenceId,
+              alreadyJoined,
+              setPresenceNotifications,
+            );
           }
 
-          // Remove the notification after 5 seconds.
-          setTimeout(() => {
+          const timeoutId = setTimeout(() => {
             setPresenceNotifications((prev) =>
-              prev.filter(function (_, i) {
-                return i !== 0;
-              }),
+              prev.slice(1),
             );
           }, 5000);
-        },
-      );
+
+          return () => {
+            clearTimeout(timeoutId);
+          };
+        }
+      };
+
+      docPresence.on('receive', handleReceive);
+      return () => {
+        docPresence.off('receive', handleReceive);
+      };
     }
-  }, [docPresence]);
+  }, [docPresence, localPresence]);
 
   return presenceNotifications.length > 0 ? (
     <div className="vz-notification">
-      {presenceNotifications.map((notification, i) => {
-        const { user, join } = notification;
-        return (
-          <div key={i} className="vz-notification-item">
+      {presenceNotifications.map(
+        ({ user, join, presenceId }) => (
+          <div
+            key={presenceId}
+            className="vz-notification-item"
+          >
             {join
               ? `${user} has joined the session.`
               : `${user} has left the session.`}
           </div>
-        );
-      })}
+        ),
+      )}
     </div>
   ) : null;
 };
