7b8c93596c79656800136f494e6953f1a927ed72
natpbs <natpbs@gmail.com>
2024-01-10 21:16:47 +0100

    #100 install react-router-dom

--- src/client/main.jsx
+++ src/client/main.jsx
@@ -1,9 +1,12 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom/client';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
-  <App />,
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
   // </React.StrictMode>,
 );
