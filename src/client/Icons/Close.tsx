9087895060131c233dfbff4eb43033c0a9043f2f
Matt Chin <chinm4@rpi.edu>
2024-01-26 19:13:25 -0500

    Added the SVG icon components
        Make a new file
        Paste the SVG
        Fix the property names with dashes,
        Add the file to the index
    
    Co-Authored-By: Darren Wong <58476988+darrenW04@users.noreply.github.com>

---
+++ src/client/Icons/Close.tsx
@@ -0,0 +1,17 @@
+export const Close = () => {
+  <svg 
+    xmlns="http://www.w3.org/2000/svg" 
+    width="24" 
+    height="24" 
+    viewBox="0 0 24 24" 
+    fill="none"
+  >
+    <path 
+      d="M6 18L18 6M6 6L18 18" 
+      stroke="#576786" 
+      strokeWidth="1.5" 
+      strokeLinecap="round" 
+      strokeLinejoin="round"
+    />
+  </svg>
+};
