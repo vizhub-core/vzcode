f3c10c6f1b6c2968630defd45730816523a4c1b6
Curran <curran.kelleher@gmail.com>
2024-02-11 09:52:50 -0500

    Progress on context refactoring

--- src/client/VZSettings.tsx
+++ src/client/VZSettings.tsx
@@ -1,55 +1,28 @@
-import { useCallback, useRef } from 'react';
+import { useCallback, useContext, useRef } from 'react';
 import { Button, Modal, Form } from './bootstrap';
-import { ThemeLabel, themes } from './themes';
-import { Username } from '../types';
-
-// const saveTimes = [
-//   { value: 1, label: '1 second' },
-//   { value: 5, label: '5 seconds' },
-//   { value: 10, label: '10 seconds' },
-//   { value: 30, label: '30 seconds' },
-// ];
+import { themes } from './themes';
+import { VZCodeContext } from './VZCodeContext';
 
 export const VZSettings = ({
-  show,
-  onClose,
-  theme,
-  setTheme,
-  username,
-  setUsername,
-
   enableUsernameField = true,
 }: {
-  show: boolean;
-  onClose: () => void;
-  theme: ThemeLabel;
-  setTheme: (theme: ThemeLabel) => void;
-  username: Username;
-  setUsername: (username: Username) => void;
-
   // Feature flag to enable/disable username field
   enableUsernameField?: boolean;
 }) => {
+  const {
+    isSettingsOpen,
+    closeSettings,
+    theme,
+    setTheme,
+    username,
+    setUsername,
+  } = useContext(VZCodeContext);
+
   const handleThemeChange = useCallback((event) => {
     const selectedValue = event.target.value;
     console.log(selectedValue);
     setTheme(selectedValue);
   }, []);
-
-  // TODO https://github.com/vizhub-core/vzcode/issues/95
-  // const handleSaveTimeChange = useCallback(
-  //   (selectedOption) => {
-  //     const time = [{ value: selectedOption.value }];
-  //     fetch('/saveTime', {
-  //       method: 'POST',
-  //       headers: {
-  //         'Content-Type': 'application/json',
-  //       },
-  //       body: JSON.stringify(time),
-  //     });
-  //   },
-  //   [],
-  // );
 
   const usernameRef = useRef<HTMLInputElement>(null);
 
@@ -57,11 +30,11 @@ export const VZSettings = ({
     setUsername(usernameRef.current?.value || '');
   }, [setUsername]);
 
-  return show ? (
+  return isSettingsOpen ? (
     <Modal
       className="vz-settings"
-      show={show}
-      onHide={onClose}
+      show={isSettingsOpen}
+      onHide={closeSettings}
       animation={false}
     >
       <Modal.Header closeButton>
@@ -104,21 +77,9 @@ export const VZSettings = ({
             Select a color theme for the editor
           </Form.Text>
         </Form.Group>
-        {/*
-          TODO https://github.com/vizhub-core/vzcode/issues/95
-          <Form.Group className="mb-3" controlId="formFork">
-          <Form.Label>Auto-Save Time</Form.Label>
-          <Select
-            options={saveTimes}
-            onChange={handleSaveTimeChange}
-          />
-          <Form.Text className="text-muted">
-            Select an auto save time
-          </Form.Text>
-        </Form.Group> */}
       </Modal.Body>
       <Modal.Footer>
-        <Button variant="primary" onClick={onClose}>
+        <Button variant="primary" onClick={closeSettings}>
           Done
         </Button>
       </Modal.Footer>
