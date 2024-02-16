584c397d1b4fe327d3ab2254e79452bb69677dbd
Marcuschan2004 <mchan.marcus@gmail.com>
2024-02-16 17:34:28 -0500

    Better Contrast
    I created a way to change the color of the "insertion point" to match the contrast of the background.

--- test/sampleDirectories/kitchenSink/index.js
+++ test/sampleDirectories/kitchenSink/index.js
@@ -1,1 +1,7 @@
-console.log('This is a test file to edit');
+var inputs = document.querySelectorAll('input, textarea');
+inputs.forEach(function (input) {
+  input.classList.add('custom-cursor');
+});
+inputs.forEach(function (input) {
+  input.style.cursor = 'pointer';
+});
