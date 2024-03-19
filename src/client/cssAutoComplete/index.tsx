dba045909cac98d1d2f6ab97cfa6300256a5b1b0
iamdanielyun <dsy3712@gmail.com>
2024-03-19 17:12:08 -0400

    starter code for css auto completion

---
+++ src/client/cssAutoComplete/index.tsx
@@ -0,0 +1,16 @@
+import {CompletionContext} from "@codemirror/autocomplete"
+
+//Basic starter code from online - will ltry to tweak this to fit our needs
+function myCompletions(context: CompletionContext) {
+  let word = context.matchBefore(/\w*/)
+  if (word.from == word.to && !context.explicit)
+    return null
+  return {
+    from: word.from,
+    options: [
+      {label: "match", type: "keyword"},
+      {label: "hello", type: "variable", info: "(World)"},
+      {label: "magic", type: "text", apply: "⠁⭒*.✩.*⭒⠁", detail: "macro"}
+    ]
+  }
+}
