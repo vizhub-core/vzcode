add5e5fee7d87552e45d853d7838d116ea8074b4
Curran <curran.kelleher@gmail.com>
2024-03-27 14:38:38 -0400

    Fix globals linting issue

--- src/client/useTypeScript/worker.ts
+++ src/client/useTypeScript/worker.ts
@@ -120,6 +120,55 @@ async function ensureFileSystemInitialized() {
   await fileSystemInitializationPromise;
 }
 
+// Be less aggressive for non-TS files,
+// e.g. files that end in .js or .jsx.
+// if (!isTS(fileName)) {
+// This code is for errors like:
+// "Binding element 'data' implicitly has an 'any' type."
+const LINT_ERROR_CODE_ANY = 7031;
+
+// This code is for errors like:
+// "Parameter 'selection' implicitly has an 'any' type.""
+const LINT_ERROR_CODE_ANY_PARAM = 7006;
+
+// This code is for errors like:
+// "Cannot find module 'd3' or its corresponding type declarations."
+const LINT_ERROR_CODE_IMPORT = 2307;
+
+// This code is for errors like:
+// "Variable 'mic' implicitly has type 'any' in some locations where its type cannot be determined."
+const LINT_ERROR_CODE_ANY_TYPE = 7034;
+
+// "Element implicitly has an 'any' type because expression
+// of type '"test"' can't be used to index type '{}'."
+const LINT_ERROR_CODE_ANY_TYPE_KEYS = 7053;
+
+// "Property 'id' does not exist on type { ... }"
+// Happens on objects with dynamic keys.
+// Not valid in TypeScript, but common in JavaScript.
+const LINT_ERROR_CODE_NON_EXISTENT_PROPERTY = 2339;
+
+// "Type 'Set<unknown>' can only be iterated through when using the '--downlevelIteration'
+// flag or with a '--target' of 'es2015' or higher."
+const LINT_ERROR_CODE_ITERATED_THROUGH = 2802;
+
+// Argument of type '{ Month: string; High: number; Temp: number; Low: number; }'
+// is not assignable to parameter of type 'never'.
+const LINT_ERROR_CODE_ASSIGNABLE_TO_NEVER = 2345;
+
+// Cannot find name 'd3'.
+const LINT_ERROR_CODE_CANNOT_FIND_NAME = 2304;
+const excludedErrorCodes = new Set([
+  LINT_ERROR_CODE_ANY,
+  LINT_ERROR_CODE_IMPORT,
+  LINT_ERROR_CODE_ANY_PARAM,
+  LINT_ERROR_CODE_ANY_TYPE,
+  LINT_ERROR_CODE_ANY_TYPE_KEYS,
+  LINT_ERROR_CODE_NON_EXISTENT_PROPERTY,
+  LINT_ERROR_CODE_ITERATED_THROUGH,
+  LINT_ERROR_CODE_ASSIGNABLE_TO_NEVER,
+]);
+
 onmessage = async ({ data }) => {
   if (debug) {
     console.log('message received');
@@ -239,69 +288,23 @@ onmessage = async ({ data }) => {
           ),
         );
 
-      // Be less aggressive for non-TS files,
-      // e.g. files that end in .js or .jsx.
-      // if (!isTS(fileName)) {
-      // This code is for errors like:
-      // "Binding element 'data' implicitly has an 'any' type."
-      const LINT_ERROR_CODE_ANY = 7031;
-
-      // This code is for errors like:
-      // "Parameter 'selection' implicitly has an 'any' type.""
-      const LINT_ERROR_CODE_ANY_PARAM = 7006;
-
-      // This code is for errors like:
-      // "Cannot find module 'd3' or its corresponding type declarations."
-      const LINT_ERROR_CODE_IMPORT = 2307;
-
-      // This code is for errors like:
-      // "Variable 'mic' implicitly has type 'any' in some locations where its type cannot be determined."
-      const LINT_ERROR_CODE_ANY_TYPE = 7034;
-
-      // "Element implicitly has an 'any' type because expression
-      // of type '"test"' can't be used to index type '{}'."
-      const LINT_ERROR_CODE_ANY_TYPE_KEYS = 7053;
-
-      // "Property 'id' does not exist on type { ... }"
-      // Happens on objects with dynamic keys.
-      // Not valid in TypeScript, but common in JavaScript.
-      const LINT_ERROR_CODE_NON_EXISTENT_PROPERTY = 2339;
-
-      // "Type 'Set<unknown>' can only be iterated through when using the '--downlevelIteration'
-      // flag or with a '--target' of 'es2015' or higher."
-      const LINT_ERROR_CODE_ITERATED_THROUGH = 2802;
-
-      // Argument of type '{ Month: string; High: number; Temp: number; Low: number; }'
-      // is not assignable to parameter of type 'never'.
-      const LINT_ERROR_CODE_ASSIGNABLE_TO_NEVER = 2345;
-
-      // Cannot find name 'd3'.
-      const LINT_ERROR_CODE_CANNOT_FIND_NAME = 2304;
-
       if (debug) {
         console.log('tsErrors');
         console.log(tsErrors);
       }
+
       tsErrors = tsErrors.filter(
-        (error: { code: number }) =>
-          error.code !== LINT_ERROR_CODE_ANY &&
-          error.code !== LINT_ERROR_CODE_IMPORT &&
-          error.code !== LINT_ERROR_CODE_ANY_PARAM &&
-          error.code !== LINT_ERROR_CODE_ANY_TYPE &&
-          error.code !== LINT_ERROR_CODE_ANY_TYPE_KEYS &&
-          error.code !==
-            LINT_ERROR_CODE_NON_EXISTENT_PROPERTY &&
-          error.code !== LINT_ERROR_CODE_ITERATED_THROUGH &&
-          error.code !==
-            LINT_ERROR_CODE_ASSIGNABLE_TO_NEVER &&
-          // If allowGlobals is true, we will allow
-          // global variables to be used without error.
-          allowGlobals
-            ? error.code !==
-              LINT_ERROR_CODE_CANNOT_FIND_NAME
-            : true,
+        (error: { code: number }) => {
+          if (
+            allowGlobals &&
+            error.code === LINT_ERROR_CODE_CANNOT_FIND_NAME
+          ) {
+            return false;
+          }
+          return !excludedErrorCodes.has(error.code);
+        },
       );
-      // }
+
       tsErrors = convertToCodeMirrorDiagnostic(tsErrors);
     }
 
