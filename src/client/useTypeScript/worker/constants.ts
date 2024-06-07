49c9e6c4d95038ea3b1f8a0cee48de68031ad71e
Pratheet.Joshi <101433332+DaCatDude@users.noreply.github.com>
2024-06-07 10:58:51 -0400

    Fix for specific typescript warning for object reassignment [code 2739] (#688).

--- src/client/useTypeScript/worker/constants.ts
+++ src/client/useTypeScript/worker/constants.ts
@@ -61,6 +61,10 @@ const LINT_ERROR_CODE_ASSIGNABLE_TO_NEVER = 2345;
 // Cannot find name 'd3'.
 export const LINT_ERROR_CODE_CANNOT_FIND_NAME = 2304;
 
+// Type '{}' is missing the following properties from type '{ indx:
+//Ignore specific TypeScript warning on object reassignment
+const LINT_ERROR_CODE_OBJ_REASSINGMENT = 2739;
+
 export const excludedErrorCodes = new Set([
   LINT_ERROR_CODE_ANY,
   LINT_ERROR_CODE_IMPORT,
@@ -70,4 +74,5 @@ export const excludedErrorCodes = new Set([
   LINT_ERROR_CODE_NON_EXISTENT_PROPERTY,
   LINT_ERROR_CODE_ITERATED_THROUGH,
   LINT_ERROR_CODE_ASSIGNABLE_TO_NEVER,
+  LINT_ERROR_CODE_OBJ_REASSINGMENT,
 ]);
