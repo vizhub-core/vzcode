03c3db69fbcf3d200dde8cbcede75b436b210d88
Curran <curran.kelleher@gmail.com>
2024-09-21 09:17:31 -0400

    Upgrade deps

--- test/sampleDirectories/kitchenSink/index.js
+++ test/sampleDirectories/kitchenSink/index.js
@@ -16,15 +16,12 @@ cyberData['test'] = 'foo';
 const initialData = [
   { x: 155, y: 386, r: 20, fill: '#5B9B31' },
   { x: 340, y: 238, r: 52, fill: '#99156D' },
-  { x: 531, y: 151, r: 20, fill: '#00FF88' },
+  { x: 531, y: 151, r: 20, fill: '#1AAD68' },
   { x: 482, y: 307, r: 147, fill: '#55AA95' },
   { x: 781, y: 91, r: 61, fill: '#0E3DFB' },
   { x: 668, y: 229, r: 64, fill: '#7300FF' },
-  // TODO add more circles
-  ```javascript
-{ x: 123, y: 456, r: 30, fill: '#FFA500' },
-{ x: 789, y: 101, r: 25, fill: '#00BFFF' },
-```,
+  { x: 123, y: 456, r: 30, fill: '#FFA500' },
+  { x: 789, y: 101, r: 25, fill: '#00BFFF' },
 ];
 
 // Add IDs to each datum
