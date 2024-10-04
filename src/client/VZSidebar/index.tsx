ce8f8975e7c3c471c9b1986abf25eb8bfd18bd9c
alizavirji <alizazv@gmail.com>
2024-10-04 10:45:47 -0400

    "Copy Files List" button

--- src/client/VZSidebar/index.tsx
+++ src/client/VZSidebar/index.tsx
@@ -38,6 +38,15 @@ import './styles.scss';
 // See https://github.com/vizhub-core/vzcode/issues/456
 const enableConnectionStatus = true;
 
+const copyFileList = (fileNames: string[]) => {
+  const fileListString = fileNames.join('\n');
+  navigator.clipboard.writeText(fileListString).then(() => {
+    alert('File list copied to clipboard!');
+  }).catch((err) => {
+    console.error('Could not copy text: ', err);
+  });
+};
+
 export const VZSidebar = ({
   createFileTooltipText = (
     <>
@@ -205,7 +214,29 @@ export const VZSidebar = ({
     }
   }, [docPresence]);
 
+
+
   // console.log(sidebarPresenceIndicators);
+  const fileNames = useMemo(() => {
+    return fileTree?.children?.map((entity) => {
+      const { path } = entity as FileTree;
+      return path;
+    }) || [];
+  }, [fileTree]);
+
+
+    useEffect(() => {
+    const copyFilesElement = document.querySelector('.copy-files-list-blurb');
+    if (copyFilesElement) {
+      copyFilesElement.addEventListener('click', () => copyFileList(fileNames));
+    }
+
+    return () => {
+      if (copyFilesElement) {
+        copyFilesElement.removeEventListener('click', () => copyFileList(fileNames));
+      }
+    };
+  }, [fileNames]);
 
   return (
     <div
