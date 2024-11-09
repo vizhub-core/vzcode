f71d6c1ee64fd349f04e9ed96563e4fb57825a27
cstecc <46802772+cstecc@users.noreply.github.com>
2024-11-08 22:29:50 -0500

    Update colorPicker.ts
    just has some bug I mentioned in status update.

--- src/client/CodeEditor/InteractiveWidgets/colorPicker.ts
+++ src/client/CodeEditor/InteractiveWidgets/colorPicker.ts
@@ -1,8 +1,9 @@
 import { InteractRule } from '@replit/codemirror-interact';
+import * as d3 from "d3"; 
 
 // Regular expression for hex colors.
 export const colorPickerRegex = /#[0-9A-Fa-f]{6}/g;
-
+/*
 // hex color picker
 // Inspired by https://github.com/replit/codemirror-interact/blob/master/dev/index.ts#L71
 // Works without quotes to support CSS.
@@ -89,3 +90,110 @@ export const colorPicker = (
 // // Inspired by https://github.com/replit/codemirror-interact/blob/master/dev/index.ts#L117
 // const rgb2Hex = (r: number, g: number, b: number): string =>
 //   '#' + r.toString(16) + g.toString(16) + b.toString(16);
+*/
+
+//perceptual hcl color
+export const colorPicker = (
+  onInteract?: () => void,
+): InteractRule => ({
+  regexp: colorPickerRegex,
+  cursor: 'pointer',
+  onClick(
+    text: string,
+    setText: (newText: string) => void,
+  ) {
+    const startingColor: string = text;
+
+    //div for my colorpicker
+ 
+    const colorpick: HTMLDivElement =
+    document.createElement('div');
+    colorpick.style.position = 'absolute'
+    colorpick.style.background = 'white'
+    colorpick.style.left = '200px'
+    colorpick.style.top = '200px'
+
+    //hcl sliders
+
+    const h: HTMLInputElement =
+    document.createElement('input');
+    h.type = 'range'
+    h.min = '0'
+    h.max = '360'
+    h.value = '0'
+    
+
+    const c: HTMLInputElement = 
+    document.createElement('input');
+    c.type = 'range'
+    c.min = '0'
+    c.max = '150' //i think this value can be change but generally should be 150
+    c.value = '0'
+ 
+
+    const l: HTMLInputElement = 
+    document.createElement('input');
+    l.type = 'range'
+    l.min = '0'
+    l.max = '100'
+    l.value = '0'
+
+    const boxcolor: HTMLDivElement =
+    document.createElement('div');
+    boxcolor.style.width = '20px'
+    boxcolor.style.height = '20px'
+    boxcolor.style.background = 'white'
+
+    const colortext: HTMLDivElement =
+    document.createElement('div');
+    colortext.textContent = "hilol"
+
+    const ok: HTMLButtonElement =
+    document.createElement('button');
+    ok.textContent = "ok"
+
+    document.body.appendChild(colorpick);
+    colorpick.appendChild(h)
+    colorpick.appendChild(c)
+    colorpick.appendChild(l)
+    colorpick.appendChild(boxcolor)
+    colorpick.appendChild(colortext)
+    colorpick.appendChild(ok)
+
+    function update() {
+      const hval = parseFloat(h.value)
+      const cval = parseFloat(c.value)
+      const lval = parseFloat(l.value)
+
+      //https://d3js.org/d3-color#hcl
+      //I guess there are some ways to do this differently but I have experience with d3 and they just give a function that does this for me
+
+      const hcld3 = d3.hcl(hval,cval,lval)
+      console.log(hcld3)
+
+      //get in rgb first idk if makes difference but seems good to have anyway
+      const rgbd3 = d3.color(hcld3).formatRgb()
+      console.log(rgbd3)
+     
+      //now can make hex for set text
+      const hexd3 = d3.color(rgbd3).formatHex()
+      console.log(hexd3)
+
+      setText(hexd3) //this is bugged idk why but works when I hardcode a hex like #a10111 but even though hexd3 is in same format it just has a bug it wont change it idk
+
+      colortext.textContent = hexd3
+      boxcolor.style.background = hexd3
+    }
+    h.addEventListener('input', update);
+    c.addEventListener('input', update);
+    l.addEventListener('input', update);
+
+    function closeok() {
+      colorpick.remove()
+    }
+
+    ok.addEventListener('click',closeok);
+
+    update();
+  },
+});
