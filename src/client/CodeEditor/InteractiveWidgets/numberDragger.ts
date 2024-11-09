21cf0a130106a5e46c6dd02d1efe96ebdd44a570
Ryan Tsang <tsangr2@rpi.edu>
2024-11-08 23:13:11 -0500

    WIP for number dragger outside editor window

--- src/client/CodeEditor/InteractiveWidgets/numberDragger.ts
+++ src/client/CodeEditor/InteractiveWidgets/numberDragger.ts
@@ -18,3 +18,61 @@ export const numberDragger = (
     if (onInteract) onInteract();
   },
 });
+
+/*
+  //WIP
+  // Error: Dragging number offscreen currently changes value randomly
+  let dragging = false;
+
+  let globalText: string;
+  let globalSetText: (newText: string) => void;
+
+
+  const handleMouseMove = (e: MouseEvent, text: string, setText: (newText: string) => void) => {
+
+    if (!dragging) return;
+    
+    const newVal = Number(text) + e.movementX;
+    // Change newVal to save current start position and non dragging position to calculate new position?
+    if (!isNaN(newVal)) {
+      setText(newVal.toString());
+      if (onInteract) onInteract();
+    }
+  };
+
+  const handleMouseUp = () => {
+    dragging = false;
+
+    window.removeEventListener('mousemove', handleMouseMoveWrapper);
+    window.removeEventListener('mouseup', handleMouseUp);
+    
+  };
+
+  const handleMouseMoveWrapper = (e: MouseEvent) => {
+    if (dragging) {
+      e.preventDefault();
+    }
+
+    handleMouseMove(e, globalText, globalSetText);
+  };
+
+  
+
+  return {
+    // Handle user drag value
+    regexp: numberDraggerRegex,
+    cursor: 'ew-resize',
+    onDrag: (text, setText, e) => {
+      dragging = true;
+      globalText = text; 
+      globalSetText = setText; 
+
+      // Listen for global events
+      window.addEventListener('mousemove', handleMouseMoveWrapper);
+      window.addEventListener('mouseup', handleMouseUp);
+    },
+  };
+};
+*/
+
+
