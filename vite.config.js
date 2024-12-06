96a9ef12e313c8a6b29d51b3ea175f57b5665835
cstecc <46802772+cstecc@users.noreply.github.com>
2024-12-05 22:35:27 -0500

    Update vite.config.js
    Added tailwindcss + autoprefixer

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import tailwindcss from 'tailwindcss';
+import autoprefixer from 'autoprefixer';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -33,6 +35,9 @@ export default defineConfig({
       scss: {
         api: 'modern-compiler',
       },
+    },
+    postcss: {
+      plugins: [tailwindcss(), autoprefixer()],
     },
   },
 });
