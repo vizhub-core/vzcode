b524d124a20495f9da87e483d235928dfd8fe712
shaun <raskshaun@gmail.com>
2025-06-25 11:59:10 -0400

    finished keyboard shortcuts design update

--- src/client/VZKeyboardShortcutsDoc.tsx
+++ src/client/VZKeyboardShortcutsDoc.tsx
@@ -11,7 +11,7 @@ export const VZKeyboardShortcutsDoc = () => {
 
   return isDocOpen ? (
     <Modal
-      className="vz-settings"
+      className="vz-keyboard-shortcuts"
       show={isDocOpen}
       onHide={closeDoc}
       animation={false}
@@ -27,99 +27,106 @@ export const VZKeyboardShortcutsDoc = () => {
           </Form.Text>
         </Form.Group>
         <Form.Group>
-          {/* <Form.Label>WINDOWS</Form.Label> */}
-          <ul>
-            <li>
-              <strong>
+          <div className="keyboard-shortcuts-list">
+            <div className="shortcut-item">
+              <div className="shortcut-keys">
                 <kbd>Ctrl</kbd> + <kbd>S</kbd>
-              </strong>{' '}
-              or{' '}
-              <strong>
+                <span className="or-text">or</span>
                 <kbd>Shift↑</kbd> + <kbd>Enter←</kbd>
-              </strong>{' '}
-              <br />
-              <span>Run and format code</span>
-            </li>
-            <li>
-              <strong>
+              </div>
+              <div className="shortcut-description">
+                Run and format code
+              </div>
+            </div>
+
+            <div className="shortcut-item">
+              <div className="shortcut-keys">
                 <kbd>Alt</kbd> + Drag on a number
-              </strong>
-              <br />
-              <span>
+              </div>
+              <div className="shortcut-description">
                 Modify the number by dragging left or right
-              </span>
-            </li>
-            <li>
-              <strong>
+              </div>
+            </div>
+
+            <div className="shortcut-item">
+              <div className="shortcut-keys">
                 <kbd>Alt</kbd> + Click on a hex color
-              </strong>
-              <br />
-              <span>
+              </div>
+              <div className="shortcut-description">
                 Open a color picker to modify the color
-              </span>
-            </li>
-            <li>
-              <strong>
+              </div>
+            </div>
+
+            <div className="shortcut-item">
+              <div className="shortcut-keys">
                 <kbd>Alt</kbd> + <kbd>W</kbd>
-              </strong>{' '}
-              <br />
-              <span>Close the current tab</span>
-            </li>
-            <li>
-              <strong>
+              </div>
+              <div className="shortcut-description">
+                Close the current tab
+              </div>
+            </div>
+
+            <div className="shortcut-item">
+              <div className="shortcut-keys">
                 <kbd>Alt</kbd> + <kbd>N</kbd>
-              </strong>{' '}
-              or{' '}
-              <strong>
-                <kbd>Ctrl</kbd> + <kbd>Shift↑</kbd> +{' '}
-                <kbd>N</kbd>
-              </strong>{' '}
-              <br />
-              <span>Open the create file modal</span>
-            </li>
-            <li>
-              <strong>
+                <span className="or-text">or</span>
+                <kbd>Ctrl</kbd> + <kbd>Shift↑</kbd> + <kbd>N</kbd>
+              </div>
+              <div className="shortcut-description">
+                Open the create file modal
+              </div>
+            </div>
+
+            <div className="shortcut-item">
+              <div className="shortcut-keys">
                 <kbd>Alt</kbd> + <kbd>PgUp↑</kbd>
-              </strong>{' '}
-              <br />
-              <span>
+              </div>
+              <div className="shortcut-description">
                 Change the active tab to the previous one
-              </span>
-            </li>
-            <li>
-              <strong>
+              </div>
+            </div>
+
+            <div className="shortcut-item">
+              <div className="shortcut-keys">
                 <kbd>Alt</kbd> + <kbd>PgDn↓</kbd>
-              </strong>{' '}
-              <br />
-              <span>
+              </div>
+              <div className="shortcut-description">
                 Change the active tab to the next one
-              </span>
-            </li>
-            <li>
-              <strong>Most VSCode Shortcuts</strong> <br />
-              See VSCode docs:{' '}
-              <a
-                href="https://code.visualstudio.com/shortcuts/keyboard-shortcuts-windows.pdf"
-                target="_blank"
-              >
-                Windows
-              </a>
-              ,{' '}
-              <a
-                href="https://code.visualstudio.com/shortcuts/keyboard-shortcuts-macos.pdf"
-                target="_blank"
-              >
-                MacOS
-              </a>
-              ,{' '}
-              <a
-                href="https://code.visualstudio.com/shortcuts/keyboard-shortcuts-linux.pdf"
-                target="_blank"
-              >
-                Linux
-              </a>
-            </li>
-          </ul>
+              </div>
+            </div>
+
+            <div className="shortcut-item">
+              <div className="shortcut-keys">
+                <strong>Most VSCode Shortcuts</strong>
+              </div>
+              <div className="shortcut-description">
+                See VSCode docs:{' '}
+                <a
+                  href="https://code.visualstudio.com/shortcuts/keyboard-shortcuts-windows.pdf"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Windows
+                </a>
+                ,{' '}
+                <a
+                  href="https://code.visualstudio.com/shortcuts/keyboard-shortcuts-macos.pdf"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  MacOS
+                </a>
+                ,{' '}
+                <a
+                  href="https://code.visualstudio.com/shortcuts/keyboard-shortcuts-linux.pdf"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Linux
+                </a>
+              </div>
+            </div>
+          </div>
         </Form.Group>
       </Modal.Body>
       <Modal.Footer>
