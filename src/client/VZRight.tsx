0662cd2615211f877e7ff286245eb4bb0ce29b08
TmgEE <etmg02@hotmail.com>
2025-07-24 23:34:53 -0400

    VZRight runtime

--- src/client/VZRight.tsx
+++ src/client/VZRight.tsx
@@ -1,42 +1,55 @@
-// Displays the Vite devserver iframe.
-// TODO test this out, think this through.
-// Purpose: support folks using VZCode as the editor
-// for their Vite-based projects.
+import React, { useEffect } from "react";
 import "./style.css";
 import "@vizhub/runtime";
 import {
   createRuntime,
-  createVizContent,
   VizHubRuntime,
 } from "@vizhub/runtime";
-import BuildWorker from "./buildWorker?worker";
-
+import BuildWorker from "./buildWorker";
 
 const enableIframe = true;
 
 export const VZRight = () => {
+  useEffect(() => {
+    // Get the iframe from the DOM
+    const iframe = document.getElementById("viz-iframe") as HTMLIFrameElement;
+
+    // Initialize the worker
+    const worker = new BuildWorker();
+
+    // Create runtime
+    const runtime: VizHubRuntime = createRuntime({
+      iframe,
+      worker,
+      setBuildErrorMessage: (error) => {
+        if (error) {
+          console.error("Build error:", error);
+        }
+      },
+    });
+
+    // Run code in the iframe
+    runtime.run({
+      files: {
+        "index.js": 'console.log("Hello from VizHub runtime!");',
+      },
+      enableHotReloading: true,
+      enableSourcemap: true,
+      vizId: "example-viz",
+    });
+
+    // Cleanup on unmount
+    return () => {
+      runtime.cleanup();
+      worker.terminate();
+    };
+  }, []);
+
   return (
     <div className="right">
       {enableIframe ? (
-        <iframe src="http://localhost:5173/"></iframe>
+        <iframe id="viz-iframe" src="http://localhost:5173/"></iframe>
       ) : null}
     </div>
   );
 };
-
-// Get the iframe from the DOM
-const iframe = document.getElementById(
-  "viz-iframe",
-) as HTMLIFrameElement;
-
-// Initialize the worker
-const worker = new BuildWorker();
-
-// runtime 
-const runtime: VizHubRuntime = createRuntime({
-  iframe,
-  worker,
-
-
-  
-})
