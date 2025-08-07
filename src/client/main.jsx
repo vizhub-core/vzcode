9b5c5189b09bed1d0b96d79ad8f2f20ef14c808b
TmgEE <etmg02@hotmail.com>
2025-08-07 18:01:58 -0400

    Update main.jsx
    revert change

--- src/client/main.jsx
+++ src/client/main.jsx
@@ -3,11 +3,6 @@ import * as ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 
-const wsProtocol = window.location.protocol === 'https:' ? 'wss://' : 'ws://';
-const socket = new WebSocket(
-  wsProtocol + window.location.hostname + ':3030/ws'
-);
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <App />
